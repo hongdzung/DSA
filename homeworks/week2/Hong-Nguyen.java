d2abfa64ac31fbf26da9befec1fb4affcb2df91b
hongdzung <67253556+hongdzung@users.noreply.github.com>
2020-06-27 20:50:41 -0700

    Update Hong-Nguyen.java
    Adding more test cases

--- homeworks/week2/Hong-Nguyen.java
+++ homeworks/week2/Hong-Nguyen.java
@@ -7,17 +7,26 @@ import java.util.Set;
 // Space complexity is O(1) because the number of letters is fixed
 
 public class RemoveDuplicate {
-    public static void main(String[] args) {
-        char[] s = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+    private static void test(char[] s) {
+        if (s == null) return;
         removeDuplicateCharacters(s);
         // s = {'h', 'e', 'l', 'o', ' ', 'w', 'r', 'l', 'd'};
         for (int i = 0; i < s.length; i++) {
-            System.out.print(s[i]);
+          System.out.print(s[i]);
         }
-        // Output: "helo wrd"
+        System.out.println("");
     }
 
+  public static void main(String[] args) {
+    char[] s = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+    test(s);
+    test("boonee".toCharArray());
+    test(null);
+    test("xx13566xxysfs".toCharArray());
+  }
+
     public static void removeDuplicateCharacters(char[] s) {
+        if (s == null) return;
         Set<Character> seen = new HashSet<>();
         for (int i = 0; i < s.length; i++) {
             if (seen.contains(s[i])) {
@@ -27,4 +36,4 @@ public class RemoveDuplicate {
             }
         }
     }
-   }
+ }
